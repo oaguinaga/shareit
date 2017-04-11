ce319493a46ec08a984cddf6bd5b8b8dc3930774
Omar Aguinaga <omar.aguinaga94@gmail.com>
2017-04-11 12:31:36 -0500

    A15: Add question and answer model

---
+++ spec/models/answer_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe Answer, type: :model do
+  let(:question) { Question.create!(title: "New Question Title", body: "New Question Body", resolve: false) }
+  let(:answer) { Answer.create!(body: 'Answer Body', question: question) }
+
+   describe "attributes" do
+     it "has body attributes" do
+       expect(answer).to have_attributes(body: 'Answer Body')
+     end
+   end
+end
