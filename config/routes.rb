480ce97630a0199cbd6660babd513a5e039837e6
Omar Aguinaga <omar.aguinaga94@gmail.com>
2017-04-20 19:47:28 -0500

    CP18: Introduction to CRUD, Add index for all posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,19 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
+  # get 'posts/index'
+  #
+  # get 'posts/show'
+  #
+  # get 'posts/new'
+  #
+  # get 'posts/edit'
+  resources :posts
+  #
+  # get 'welcome/index'
+  #
+  # get 'welcome/about'
 
-  get 'welcome/about'
-
+  get 'about' => 'welcome#about'
+  
   root 'welcome#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
