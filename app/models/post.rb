c79ad3fa830971c6ad569c4b5da6873de1a4c713
Omar Aguinaga <omar.aguinaga94@gmail.com>
2017-05-23 12:15:08 -0500

    A31: Add Favorite and send mail to newly created posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,6 +6,8 @@ class Post < ApplicationRecord
   has_many :votes, dependent: :destroy
   has_many :favorites, dependent: :destroy
 
+  after_create :create_favorite
+
   default_scope { order('rank DESC') }
   scope :ordered_by_title, -> { order('title DESC')}
   scope :ordered_by_reverse_created_at, -> { order('created_at ASC') }
@@ -31,5 +33,11 @@ class Post < ApplicationRecord
     age_in_days = (created_at - Time.new(1970,1,1)) / 1.day.seconds
     new_rank = points + age_in_days
     update_attribute(:rank, new_rank)
+  end
+
+  def create_favorite
+    Favorite.create(post: self, user: self.user)
+    FavoriteMailer.new_post(self).deliver.now
+
   end
 end
