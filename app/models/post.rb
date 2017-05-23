4867247d146e6889773bc2f8a1b3812a286ceb3c
Omar Aguinaga <omar.aguinaga94@gmail.com>
2017-05-23 10:22:22 -0500

    A30: Add automatic upvote to post created by some user

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,6 +5,8 @@ class Post < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :votes, dependent: :destroy
 
+  after_create :create_vote
+
   default_scope { order('rank DESC') }
   scope :ordered_by_title, -> { order('title DESC')}
   scope :ordered_by_reverse_created_at, -> { order('created_at ASC') }
@@ -30,5 +32,11 @@ class Post < ApplicationRecord
     age_in_days = (created_at - Time.new(1970,1,1)) / 1.day.seconds
     new_rank = points + age_in_days
     update_attribute(:rank, new_rank)
+  end
+
+  private
+
+  def create_vote
+    user.votes.create(value: 1, post: self)
   end
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -70,12 +70,25 @@ RSpec.describe Post, type: :model do
         post.votes.create!(value: 1)
         expect(post.rank).to eq (old_rank + 1)
       end
-      
+
       it "updates the rank when a down vote is created" do
         old_rank = post.rank
         post.votes.create!(value: -1)
         expect(post.rank).to eq (old_rank - 1)
       end
+    end
+
+    describe "#create_vote" do
+      it "excecutes create_vote when a post is created" do
+        post = topic.posts.new(title: RandomData.random_sentence, body: RandomData.random_sentence, user: user)
+        expect(post).to receive(:create_vote)
+        post.save
+      end
+
+      it 'associates the vote with the owner of the post' do
+        expect(post.votes.first.user).to eq(post.user)
+      end
+
     end
   end
 end
